e0a541c78f35a17d61e8e90b6d5cb79e63e2db3b
Jermaine Cheng <jermaine.zhimin@gmail.com>
2019-12-15 10:13:54 -0800

    Setting up repro

---
+++ README.md
@@ -0,0 +1,6 @@
+# Testing Library Reproduction Template
+
+Template repository for bug reports to @testing-library/dom,
+@testing-library/react, and @testing-library/jest-dom
+
+You can also [use this on codesandbox](http://codesandbox.io/s/github/testing-library/dom-testing-library-template)
---
+++ src/index.jsx
@@ -0,0 +1,27 @@
+// index.js
+import React from 'react'
+import { withRouter } from 'react-router'
+import { Link, Route, Switch } from 'react-router-dom'
+
+const About = () => <h1>You are on the about page</h1>
+const Home = () => <h1>You are home</h1>
+const NoMatch = () => <h1>404 Not Found</h1>
+
+export const LocationDisplay = withRouter(({ location }) => (
+	<div data-testid="location-display">{location.pathname}</div>
+))
+
+export const App = () => {
+	return (
+		<div>
+			<Link to="/">Home</Link>
+			<Link to="/about">About</Link>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route path="/about" component={About} />
+				<Route component={NoMatch} />
+			</Switch>
+			<LocationDisplay />
+		</div>
+	)
+}
